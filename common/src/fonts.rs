7874a5f465bcd776dfa17b6750e4171d8195cb10
Luca Versari <veluca93@gmail.com>
2025-09-27 09:57:38 +0200

    Add Arabic fonts + missing sqlx queries.

--- common/src/fonts.rs
+++ common/src/fonts.rs
@@ -6,6 +6,8 @@ pub fn embedded_font_files() -> impl Iterator<Item = &'static [u8]> {
         include_bytes!("../../fonts/lmroman-bold.ttf"),
         include_bytes!("../../fonts/lmroman-italic.ttf"),
         include_bytes!("../../fonts/lmroman-regular.ttf"),
+        include_bytes!("../../fonts/majalla.ttf"),
+        include_bytes!("../../fonts/majallab.ttf"),
     ]
     .into_iter()
 }
